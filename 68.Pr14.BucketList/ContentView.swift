77a27fb7337e617e99e606ccad976e555ef6457d
Valentin <pamerra@mail.ru>
2025-08-28 22:34:56 +0300

    68.3/12. Демонстрация отображения элементов в зависимости от выполнения условия

--- 68.Pr14.BucketList/ContentView.swift
+++ 68.Pr14.BucketList/ContentView.swift
@@ -9,17 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Read and Write") {
-            let data = Data("test message".utf8)
-            let url = URL.documentsDirectory.appending(path: "message.text")
-            
-            do {
-                try data.write(to: url, options: [.atomic, .completeFileProtection])
-                let input = try String(contentsOf: url, encoding: .utf8)
-                print(input)
-            } catch {
-                print(error.localizedDescription)
-            }
+        if Bool.random() {
+            Rectangle()
+        } else {
+            Circle()
         }
     }
 }
