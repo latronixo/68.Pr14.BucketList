0b7a0f73fb8743936b1fee2d17f49e85122d3451
Valentin <pamerra@mail.ru>
2025-08-28 21:18:55 +0300

    Initial Commit

---
+++ 68.Pr14.BucketList/_8_Pr14_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8_Pr14_BucketListApp.swift
+//  68.Pr14.BucketList
+//
+//  Created by Валентин on 28.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct _8_Pr14_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
