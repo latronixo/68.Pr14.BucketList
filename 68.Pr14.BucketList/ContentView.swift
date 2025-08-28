48d8a22350f27ba6a8cb3e51ba9ee140bf54fe60
Valentin <pamerra@mail.ru>
2025-08-28 22:52:22 +0300

    69.4/13. Демонстрация использования карты Map() - со спутника

--- 68.Pr14.BucketList/ContentView.swift
+++ 68.Pr14.BucketList/ContentView.swift
@@ -5,42 +5,17 @@
 //  Created by Валентин on 28.08.2025.
 //
 
+import MapKit
 import SwiftUI
 
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
-}
-
 struct ContentView: View {
-    enum LoadingState {
-        case loading, success, failed
-    }
     
-    @State private var loadingState = LoadingState.loading
+    
     
     var body: some View {
-        switch loadingState {
-        case .loading :
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
-        }
+        Map()
+            .mapStyle(.imagery)
+        
     }
 }
 
