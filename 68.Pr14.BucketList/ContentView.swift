8ececf3e7cc7638a586501b397588ffb5c630530
Valentin <pamerra@mail.ru>
2025-08-28 22:09:49 +0300

    68.1/12. Для сортировки массива со структурами нужно указать в замыкании правило для сортировки при помощи перегрузки оператора внутри структуры

--- 68.Pr14.BucketList/ContentView.swift
+++ 68.Pr14.BucketList/ContentView.swift
@@ -7,20 +7,24 @@
 
 import SwiftUI
 
-struct User: Identifiable {
+struct User: Comparable, Identifiable {
     let id = UUID()
     var firstName: String
     var lastName: String
+    
+    //lhs - left hand side
+    //rhs - right hand side
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
 }
 
 struct ContentView: View {
     let users = [
         User(firstName: "Максим", lastName: "Щегловский"),
-        User(firstName: "Татьяна", lastName: "Зенина"),
+        User(firstName: "Руслан", lastName: "Мазманян"),
         User(firstName: "Юлия", lastName: "Зеленская"),
-    ].sorted() {
-        $0.lastName < $1.lastName
-    }
+    ].sorted()
     
     var body: some View {
         List(users) { user in
