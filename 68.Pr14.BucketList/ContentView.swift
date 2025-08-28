7f72159cf112149ed265ff05c6dc194c8e2759cb
Valentin <pamerra@mail.ru>
2025-08-28 23:16:16 +0300

    69.4/12. Карта с Лондоном в начальной позиции

--- 68.Pr14.BucketList/ContentView.swift
+++ 68.Pr14.BucketList/ContentView.swift
@@ -9,13 +9,16 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    
-    
+    let position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
     
     var body: some View {
-        Map(interactionModes: [.rotate, .zoom])
+        Map(initialPosition: position)
             .mapStyle(.hybrid(elevation: .realistic))
-        
     }
 }
 
