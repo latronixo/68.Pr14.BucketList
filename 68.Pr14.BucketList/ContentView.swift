5961a818bae361e6284aac23bd2a3862319b6014
Valentin <pamerra@mail.ru>
2025-08-29 11:36:42 +0300

    69.4/12. Можно устанавливать маркеры на карте

--- 68.Pr14.BucketList/ContentView.swift
+++ 68.Pr14.BucketList/ContentView.swift
@@ -8,42 +8,27 @@
 import MapKit
 import SwiftUI
 
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
-    @State private var position = MapCameraPosition.region(
-        MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-        )
-    )
+    let locations = [
+        Location(name: "Букингемский дворец", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Лондонский тауэр", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
     
     var body: some View {
-        Map(position: $position)
-            .mapStyle(.hybrid(elevation: .realistic))
-            .onMapCameraChange(frequency: .continuous) { context in
-                print(context.region)
+        VStack {
+            Map {
+                ForEach(locations) { location in
+                    Marker(location.name, coordinate: location.coordinate)
+                }
             }
-        
-        HStack {
-            Button("Paris") {
-                position = MapCameraPosition.region(
-                    MKCoordinateRegion(
-                        center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                    )
-                )
-            }
-            
-            Button("Tokyo") {
-                position = MapCameraPosition.region(
-                    MKCoordinateRegion(
-                        center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                    )
-                )
-            }
-            
         }
-    }
+     }
 }
 
 #Preview {
