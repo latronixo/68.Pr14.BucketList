79a68d9b7656170ae6846b51348175c59fb8d110
Valentin <pamerra@mail.ru>
2025-08-29 18:08:19 +0300

    71.10/12. В описании мест википедии мы теперь показываем terms с описанием

--- 68.Pr14.BucketList/Views/EditView.swift
+++ 68.Pr14.BucketList/Views/EditView.swift
@@ -39,7 +39,7 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
-                            Text("Page description here")
+                            Text(page.description)
                                 .italic()
                         }
                     case .failed:
