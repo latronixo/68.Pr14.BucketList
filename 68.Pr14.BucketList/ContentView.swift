1e7b70d2403279c12156bcd52b556b703a59a73b
Valentin <pamerra@mail.ru>
2025-08-29 12:36:45 +0300

    69.5/12. Пример функции, выполняющей аутентификацию по биометрии (для прохождения в симуляторе нажать Cmd + Option + M)

--- 68.Pr14.BucketList/ContentView.swift
+++ 68.Pr14.BucketList/ContentView.swift
@@ -5,31 +5,39 @@
 //  Created by Валентин on 28.08.2025.
 //
 
-import MapKit
+import LocalAuthentication
 import SwiftUI
 
-struct Location: Identifiable {
-    let id = UUID()
-    var name: String
-    var coordinate: CLLocationCoordinate2D
-}
-
 struct ContentView: View {
-    let locations = [
-        Location(name: "Букингемский дворец", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Лондонский тауэр", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
+   @State private var isUnlocked = false
     
     var body: some View {
         VStack {
-            MapReader { proxy in
-                Map()
-                    .onTapGesture { position in
-                        if let coordinate = proxy.convert(position, from: .local) {
-                            print(coordinate)
-                        }
-                    }
+            if  isUnlocked {
+                Text("Unlocked")
+            } else{
+                Text("Locked")
             }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We neen to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true   //успешная аутентификация
+                } else {
+                    //ошибка аутентификации
+                }
+            }
+        } else {
+            // на данном устройстве отсутствует возможность аутентификации по биометрии
         }
     }
 }
