ef59667ba5bff9a4d428ca5a4d86f465d35a5279
Valentin <pamerra@mail.ru>
2025-08-29 13:39:06 +0300

    70.7/12. Сделал перегрузку оператора == для возможности сравнивания маркеров на равенство по их id

--- Location.swift
+++ Location.swift
@@ -22,5 +22,9 @@ struct Location: Codable, Equatable, Identifiable {
     #if DEBUG
     static let example = Location(id: UUID(), name: "Букингемский дворец", description: "Освещено более чем 40 тыс лампочками", latitude: 51.501, longitude: -0.141)
     #endif
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
 
